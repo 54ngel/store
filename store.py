cd2f45eff7420c83ca30ef121e2df1cfbd1fcb0d
Krzysztofr1 <krzysztof.rusel@infotech.edu.pl>
2024-01-04 14:25:04 +0100

    customer

--- store.py
+++ store.py
@@ -6,5 +6,12 @@ class Product:
         
 class Order:
     def __init__(self, productName, productAmount):
-        
-        
+
+class Customer:
+    def __init__(self, name, surename, adress, ):
+        self.name = name
+        self.surename = surename
+        self.adress = adress
+
+    #def CustomerOrders(self):
+        #for i in
